743aaa2db4d1c122e2d92ef6e03ac90d7da7c02a
Mazharul alam <playerc950@gmail.com>
2020-04-01 18:46:27 +0600

    add models and database

---
+++ app/Question.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    protected $fillable = [
+        'topic_id','question','board_id','option1','option2','option3','option4','correct_option','tag','details'
+    ];
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    {{-- required css --}}
+    <link rel="stylesheet" href="{{ asset('assets') }}/css/app.min.css">
+
+    {{-- required js --}}
+    <script src="{{ asset('assets') }}/js/vendors.min.js"></script>
+    <script src="{{ asset('assets') }}/js/app.min.js"></script>
+
+    <title>ICT - Add questions</title>
+</head>
+<body>
+
+    {{-- custom js --}}
+    <script src="{{ asset('assets') }}/js/custom.js"></script>
+</body>
+</html>
